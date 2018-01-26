f793b8deb666d1c6e07e5971e97687b554e53a0c
mttcug <mttcug@163.com>
2018-01-26 12:08:55 +0800

    koa渲染angular5

--- serve/app.js
+++ serve/app.js
@@ -1,69 +1,73 @@
+/**
+ * Created by dongfangyiheng on 2018/1/25.
+ */
 var koa = require('Koa');
-var router = require('koa-router')();
-const  serve = require("koa-static");
+var Router = require('koa-router');
 var bodyParser = require('koa-bodyparser');
-var fs = require('fs');
+var views=require('koa-views');
+var render=require('koa-render');
+var nunjucks = require('nunjucks');
+var static = require('koa-static');
+nunjucks.configure('views', { autoescape: true });
+
 
 
 var app = new koa();
-app.use(bodyParser());
-app.use(router.routes());
+var router = new Router();
+
+app
+  .use(bodyParser())
+  .use(static('dist'))
+  .use(views('dist', {map: {html: 'nunjucks'}}))
+  .use(router.allowedMethods())
+  .use(router.routes());
 
 
-router.get('/', async (ctx, next) => {
-  ctx.response.body =
-  '<h1>Index</h1> <form action="/login" method="post"> '
-  +
-  '<p>Name: <input name="name"></p>'
-  +
-  ' <p>Password: <input name="password" type="password"></p> '
-  +
-  '<p><input type="submit" value="Submit"></p>'
-  +
-  ' </form>';
-});
-
-
-router.post('/login', async (ctx, next) => {
-  let
-    name = ctx.request.body.name || '',
-  password = ctx.request.body.password || '';
-
-console.log(ctx.request);
-if (name === 'koa' && password === '12345') {
-  ctx.body = "Success"
-} else {
-  ctx.body = "Login error"
-
-}
-});
-/*const LOAD_HTML = function() {
-  return new Promise(function (resolve, reject) {
-    fs.readFile('./dist/index.html', {'encoding': 'utf8'}, function (err, data) {
-      if(err) return reject(err);
-      resolve(data);
-    });
+router.get('/', async function (ctx, next) {
+  await ctx.render('index.html',{
+    data:123
   });
-};
-
-router.get('/', function *(next) {
-  if (this.request.url.startsWith("/api")) {
-    yield next;
-  } else {
-    this.body = yield LOAD_HTML();
-  }
 });
 
-app.use(router.routes());*/
+/*router.get('/', async (ctx, next) => {
+ ctx.response.body =
+ '<h1>Index</h1> <form action="/login" method="post"> '
+ +
+ '<p>Name: <input name="name"></p>'
+ +
+ ' <p>Password: <input name="password" type="password"></p> '
+ +
+ '<p><input type="submit" value="Submit"></p>'
+ +
+ ' </form>';
+ });
+
+
+ router.post('/login', async (ctx, next) => {
+ let
+ name = ctx.request.body.name || '',
+ password = ctx.request.body.password || '';
+
+ console.log(ctx.request);
+ if (name === 'koa' && password === '12345') {
+ ctx.body = "Success"
+ } else {
+ ctx.body = "Login error"
+
+ }
+ });*/
+
+
 
 /*app.use(async ctx => {
-  ctx.body='mtt start666';
-});*/
+ ctx.body='mtt start666';
+ });*/
 
 //app.use(indexRouter.routes(),indexRouter.allowedMethods());
 
 app.listen(3002,function(){
   console.log('the server is listening on port 3000');
 });
+
 
 
